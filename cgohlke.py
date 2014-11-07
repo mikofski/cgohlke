0847bf9403d95b2e5a38d48a1c376998734ca1d0
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2014-11-07 01:52:54 -0800

    add arg parser

--- cgohlke.py
+++ cgohlke.py
@@ -4,11 +4,20 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import argparse
+import os
 import sys
+import platform
+from urllib import urlretrieve
+import pprint
 
 PATTERN = 'javascript:dl\(\[(?P<ml>\d+(?:,\d+)+)\], "(?P<mi>.+)"'
 CGOHLKE_URL = 'http://www.lfd.uci.edu/~gohlke/pythonlibs/'
 VERSION = '0.1'
+BUILD_DIR = os.path.expanduser(os.path.join('~','.cgohlke'))
+PYVERSION = 'py%d.%d' % (sys.version_info.major, sys.version_info.minor)
+WIN_AMD64 = 'win-amd64'
+WIN32 = 'win32'
+MACHINE = WIN_AMD64 if platform.machine() == 'AMD64' else WIN32
 
 
 def decode_link(encoded_link):
@@ -33,57 +42,67 @@ def get_cgohlke_pkgs(cgohlke_url=CGOHLKE_URL):
         grp = item.get('id')
         if grp and grp in index:
             pkgs[grp] = []
-            print 'group: %s' % grp
+            #print 'group: %s' % grp
             pkg = {}
             for subitem in item.parent.find_all('a'):
                 onclick_event = subitem.get('onclick')
                 if onclick_event:
                     group = subitem.parent.parent.parent.contents[0]['id']
-                    assert group == grp
-                    fullname = subitem.string[:-4]
-                    name, version, pyversion = fullname.rsplit(u'\u2011', 2)
-                    pkg['fullname'] = fullname.replace(u'\u2011', '-')
-                    print 'package: %s' % pkg['fullname']
+                    #assert group == grp
+                    fullname = subitem.string[:-4].replace(u'\u2011', '-')
+                    pkg['fullname'] = fullname
+                    name, pyversion = fullname.rsplit('-', 1)
+                    name, machine = name.rsplit('.', 1)
+                    name, version = name.rsplit('-', 1)
+                    #print 'package: %s' % pkg['fullname']
                     pkg['name'] = name
-                    pkg['version'] = version
+                    if version.endswith(WIN_AMD64):
+                        pkg['machine'] = WIN_AMD64
+                        pkg['version'] = version[:len(WIN_AMD64)]
+                    else:
+                        pkg['machine'] = WIN32
+                        pkg['version'] = version[:len(WIN32)]
                     pkg['pyversion'] = pyversion
                     encoded_link = re.match(PATTERN, onclick_event).groupdict()
                     pkg['link'] = decode_link(encoded_link)
                     pkgs[group].append(pkg)
     return pkgs
 
+PKGS = get_cgohlke_pkgs()
+
+def search(pkgs):
+    print 'search results:'
+    for pkg in pkgs:
+        if pkg in PKGS:
+            pprint.pprint(PKGS[pkg])
+
+
+def install(pkgs):
+    for pkg in pgks:
+        if pkg in PKGS:
+            pyvers = [(p['machine'], p['pyversion']) for p in pkg]
+            idx = pyvers.index((MACHINE, PYVERSION))
+            link = pkg[idx]['link']
+            filename, headers = urlretrieve(link, os.join(BUILD_DIR, link))
+    return filename, headers
 
 if __name__ == '__main__':
     # parse the input arguments from the command line, defaults are None
-    parser = argparse.ArgumentParser(description='install cgohlke python libs',
+    parser = argparse.ArgumentParser(prog='cgohlke',
+                                     description='install cgohlke python libs',
                                      epilog='Mark Mikofski, copyright (c) 2014')
     # show version info
     parser.add_argument('--version', '-v', action='version',
-                        version='%(prog)s version: %(version)s' %
+                        version='%(prog)s-%(version)s' %
                         {"prog": parser.prog, "version": VERSION})
-    # input pvsim data filename
-    parser.add_argument('--pvsim-data', '-d', type=str, default=PVSIMDATA,
-                        help="PVSim data file [%(default)s]")
-    parser.add_argument('--year', '-y', type=int, default=YEAR,
-                        help="simulation year [%(default)s]")
-    # select output file
-    parser.add_argument('--output-filename', '-f', type=str,
-                        default=OUTPUTFILENAME,
-                        help="output data file [%(default)s]")
-    # select optimization flag
-    parser.add_argument('--optimize', '-o', type=int, choices=[0, 1],
-                        help='optimization flags')
-    # select losses flag
-    parser.add_argument('--loss-contributors', '-l', type=int,
-                        choices=range(5), help='loss contributor flags')
-    # select a JSON file with all system inputs
-    parser.add_argument('--system-inputs', '-s', type=str,
-                        help="JSON file with additional inputs")
-    # turn on multiprocessing
-    parser.add_argument('--parallel', '-p',  action="store_true",
-                        help="activate multiprocessing")
-    # max tasks per child
-    parser.add_argument('--max-tasks', '-t', type=int,
-                        help="max tasks per worker before being freed")
-    # TODO: add some more of Ben's favorite input arguments here
+    subparsers = parser.add_subparsers(help='sub-command help')
+    # create the parser for the "search" command
+    parser_search = subparsers.add_parser('search', help='search for package')
+    parser_search.add_argument('pkgs', nargs='+', help='name of package to find')
+    parser_search.set_defaults(func=search)
+    # create the parser for the "install" command
+    parser_install = subparsers.add_parser('install', help='install package')
+    parser_install.add_argument('pkgs', nargs='+', help='name of package to install')
+    parser_install.set_defaults(func=install)
     args = parser.parse_args()  # parse arguments
+    args.func(args.pkgs)
