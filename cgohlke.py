41530f2f4717a5eccffe422a5f43f347e45c62b6
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2014-11-07 17:38:00 -0800

    use pip --use-wheel --no-index and --find-links commands, use shift and create log

--- cgohlke.py
+++ cgohlke.py
@@ -8,6 +8,7 @@ import os
 import sys
 import platform
 import pprint
+import csv
 import logging
 
 # basic logging configuration
@@ -17,7 +18,8 @@ logging.basicConfig(level=logging.DEBUG, format=LOG_FMT)
 PATTERN = 'javascript:dl\(\[(?P<ml>\d+(?:,\d+)+)\], "(?P<mi>.+)"'
 CGOHLKE_URL = 'http://www.lfd.uci.edu/~gohlke/pythonlibs/'
 VERSION = '0.1'
-BUILD_DIR = os.path.expanduser(os.path.join('~','.cgohlke'))
+BUILD_DIR = os.path.expanduser(os.path.join('~', '.cgohlke'))
+BUILD_LOG = os.path.join(BUILD_DIR, 'cgohlke.log')
 PYVERSION = 'py%d.%d' % (sys.version_info.major, sys.version_info.minor)
 WIN_AMD64 = 'win-amd64'
 WIN32 = 'win32'
@@ -74,18 +76,28 @@ PKGS = get_cgohlke_pkgs()
 
 def search(pkgs):
     print 'search results:'
-    for pkg in pkgs:
-        if pkg in PKGS:
-            pprint.pprint(PKGS[pkg])
+    for pkg, _ in pkgs:
+        if pkg not in PKGS:
+            continue
+        pprint.pprint(PKGS[pkg])
 
 
 def install(pkgs):
     for pkg in pkgs:
+        if len(pkg) == 2:
+            pkg, ver = pkg
+        else:
+            pkg = pkg[0]
+            ver = None
         if pkg not in PKGS:
             continue
-        pyvers = [(p['machine'], p['pyversion']) for p in PKGS[pkg]]
+        if ver:
+            pyvers = [(p['machine'], p['pyversion'], p['version']) for p in PKGS[pkg]]
+            idx = pyvers.index((MACHINE, PYVERSION, ver))
+        else:
+            pyvers = [(p['machine'], p['pyversion']) for p in PKGS[pkg]]
+            idx = pyvers.index((MACHINE, PYVERSION))
         logging.debug('packages:\n%s', pyvers)
-        idx = pyvers.index((MACHINE, PYVERSION))
         link = CGOHLKE_URL + PKGS[pkg][idx]['link']
         filename = PKGS[pkg][idx]['fullname'] + '.exe'
         if os.path.exists(os.path.join(BUILD_DIR, filename)):
@@ -99,15 +111,25 @@ def install(pkgs):
         r.close()
 
 
+def init_log(*args):
+    files = os.listdir(os.path.expanduser(BUILD_DIR))
+    pkgs = [os.path.join(BUILD_DIR, f).replace('C:/','/c/').replace('C:\\','/c/').replace('c:/','/c/').replace('c:\\','/c/').replace('\\','/') for f in files if f.endswith('.exe')]
+    with open(BUILD_LOG, 'w') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter='\n')
+        spamwriter.writerow(pkgs)
+    logging.debug('(re)initialized log: %s', BUILD_LOG)
+
+
 if __name__ == '__main__':
     # parse the input arguments from the command line, defaults are None
     parser = argparse.ArgumentParser(prog='cgohlke',
                                      description='install cgohlke python libs',
                                      epilog='Mark Mikofski, copyright (c) 2014')
     # show version info
-    parser.add_argument('--version', '-v', action='version',
+    parser.add_argument('-v', '--version', action='version',
                         version='%(prog)s-%(version)s' %
                         {"prog": parser.prog, "version": VERSION})
+    parser.set_defaults(pkgs='')
     subparsers = parser.add_subparsers(help='sub-command help')
     # create the parser for the "search" command
     parser_search = subparsers.add_parser('search', help='search for package')
@@ -117,7 +139,11 @@ if __name__ == '__main__':
     parser_install = subparsers.add_parser('install', help='install package')
     parser_install.add_argument('pkgs', nargs='+', help='name of package to install')
     parser_install.set_defaults(func=install)
+    # create the parser for the "init" command
+    parser_init = subparsers.add_parser('init', help='(re)initialize log')
+    parser_init.set_defaults(func=init_log)
     args = parser.parse_args()  # parse arguments
+    pkgs = [re.split('==|>=', pkg) for pkg in args.pkgs]
     logging.debug('args: %s', args)
-    logging.debug('packages: %s', args.pkgs)
-    args.func(args.pkgs)
+    logging.debug('packages: %s', pkgs)
+    args.func(pkgs)
